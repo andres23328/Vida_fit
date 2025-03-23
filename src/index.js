f7ed4f35ea04c606550216cc0e07e6b188ce1e5d
andres23328 <ardila23328@gmail.com>
2025-03-23 18:19:43 -0500

    Proyecto terminado

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <GoogleOAuthProvider clientId="1086203109768-qrtvmqag09t1mac3gfork0oeung1lrt7.apps.googleusercontent.com">
+            <App />
+        </GoogleOAuthProvider>
+    </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
